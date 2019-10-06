c8252cce3e609e23104b86d068661f6c860df797
donghyeon-dev <donghyeon-dev@gmail.com>
2019-10-06 12:56:08 +0900

    Modify Model field

--- users/models.py
+++ users/models.py
@@ -5,5 +5,21 @@ from django.db import models
 
 class User(AbstractUser):
 
-    bio = models.TextField(default="")
+    """ Custom User Model """
 
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICE = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICE, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
+    """ DB상에서 black와 null은 차이가 있음 ===>>> null=True일 경우에는 유저등록시 required로 나오기 때문에 blank 사용 """
