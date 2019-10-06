cb2bfc625660a272d6cf62a78920341df4f6bf95
donghyeon-dev <donghyeon-dev@gmail.com>
2019-10-06 17:56:35 +0900

    Extend UserAdmin pannel

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,12 @@ from . import models
 
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+
+    """ Custom User Admin"""
+
+    list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    list_filter = ("language", "currency", "superhost")
+
+
+""" 5라인의 다른 형식 =  admin.site.register(models.User, CustomUserAdmin.)
+admin패널의 User가 customUserADmin. class를 control"""
--- users/models.py
+++ users/models.py
@@ -16,10 +16,28 @@ class User(AbstractUser):
         (GENDER_FEMALE, "Female"),
         (GENDER_OTHER, "Other"),
     )
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
 
+    LANGUAGE_CHOICE = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICE = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(
         choices=GENDER_CHOICE, max_length=10, null=True, blank=True
     )
     bio = models.TextField(default="", blank=True)
-    """ DB상에서 black와 null은 차이가 있음 ===>>> null=True일 경우에는 유저등록시 required로 나오기 때문에 blank 사용 """
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICE, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICE, max_length=3, null=True, blank=True
+    )
+
+    superhost = models.BooleanField(default=False)
+    """ DB상에서 black와 null은 차이가 있음 => null=True일 경우에는 유저등록시 required로 나오기 때문에 blank사용 """
+    """ model에서 새로운 모델을 작업한 후 makemigration > migrate  """
